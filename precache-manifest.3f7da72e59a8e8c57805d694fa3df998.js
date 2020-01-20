7015ec718f4228c1827f25104026b4b6289cc86c
Michael Neely <michael@michaelneely.net>
2020-01-19 19:54:12 -0500

    Updates

---
+++ precache-manifest.3f7da72e59a8e8c57805d694fa3df998.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae2ef5ed88f840318f687acc3ddb166",
+    "url": "/weatyher-app-react/index.html"
+  },
+  {
+    "revision": "bcc83f6cbe63d2dd0f54",
+    "url": "/weatyher-app-react/static/css/main.39b6787a.chunk.css"
+  },
+  {
+    "revision": "30716b6a7c7f16f09fdb",
+    "url": "/weatyher-app-react/static/js/2.38222fd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatyher-app-react/static/js/2.38222fd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcc83f6cbe63d2dd0f54",
+    "url": "/weatyher-app-react/static/js/main.c9717038.chunk.js"
+  },
+  {
+    "revision": "41fdb35437ef8e7ebde5",
+    "url": "/weatyher-app-react/static/js/runtime-main.a7172b27.js"
+  },
+  {
+    "revision": "5b1c73ac8cda4cb6ebccf60121a8fd66",
+    "url": "/weatyher-app-react/static/media/cold-weather.5b1c73ac.png"
+  },
+  {
+    "revision": "a5ba04c8cff559bffbb9e16204a26cde",
+    "url": "/weatyher-app-react/static/media/warm-weather.a5ba04c8.png"
+  }
+]);
